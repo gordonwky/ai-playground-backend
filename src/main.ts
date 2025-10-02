01ebb36c4a656a762dd5c2860455ef30e780463e
Gordon Wong <gordon.wong@vretta.com>
2025-10-01 22:30:59 -0400

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
     app.enableCors({
     origin: process.env.FRONTEND_URL ?? 'http://localhost:3000',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE, OPTIONS',
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3001);
